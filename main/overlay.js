e5fba7a168f1cbfd0dfc5c7d670a0f94b79d6326
ByFor4 <144042639+ByFor4@users.noreply.github.com>
2025-05-15 14:27:20 +0300

    com4

--- main/overlay.js
+++ main/overlay.js
@@ -1,19 +1,31 @@
 const overlay = document.getElementById('overlay');
+const page_legend = document.getElementById('page_legend');
+const page_history = document.getElementById('page_history');
 
-function opacity_on() {
+
+function opacity_on_l() {
     overlay.style.opacity = 100;
+    page_legend.style.display = "block"
+    page_history.style.display = "none"
   }
+
+function opacity_on_h() {
+    overlay.style.opacity = 100;
+    page_history.style.display = "block"
+    page_legend.style.display = "none"
+}
 
 function opacity_off() {
     overlay.style.display = "none"
-    
+    page_legend.style.display = "none"
+    page_history.style.display = "none"
 }
 
 document.querySelectorAll(".legend").forEach(side_menu_el => {
     side_menu_el.addEventListener("click", () => {
         console.log("clik")
         overlay.style.display = "block";
-        setTimeout(opacity_on, 100)
+        setTimeout(opacity_on_l, 100)
     });
 });
 
@@ -21,7 +33,7 @@ document.querySelectorAll(".history").forEach(side_menu_el => {
     side_menu_el.addEventListener("click", () => {
         console.log("clik")
         overlay.style.display = "block";
-        setTimeout(opacity_on, 100)
+        setTimeout(opacity_on_h, 100)
     });
 });
 
