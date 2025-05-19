0fa35ca342da3a194325735f278673e702130ab6
ByFor4 <144042639+ByFor4@users.noreply.github.com>
2025-05-19 12:57:56 +0300

    prot legend

--- test/script.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById('showButton');
-const gallery = document.getElementById('gallery');
-const overlay = document.getElementById('overlay');
-
-button.addEventListener('click', () => {
-  gallery.classList.toggle('open');
-  overlay.style.opacity = gallery.classList.contains('open') ? '1' : '0';
-});
